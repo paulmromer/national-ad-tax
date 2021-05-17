75dc0b7476212bd1fc22ed34d2133ba1d07e1415
Paul Romer <paul@paulromer.net>
2021-05-17 15:50:52 -0400

    include support for Safari on desktop by skipping call to matplotlib

--- js/pyodide.js
+++ js/pyodide.js
@@ -1,16 +1,45 @@
+// get the browser
+const browser = get_browser().name.toLowerCase();  
+
 async function loadPyodideAndPackages(){
+
+    // load pyodide
     await loadPyodide({ indexURL : 'https://cdn.jsdelivr.net/pyodide/v0.17.0/full/' });
-    await self.pyodide.loadPackage('matplotlib');
-    await self.pyodide.runPython("import matplotlib.pyplot as plt")
-    await self.pyodide.runPython("from matplotlib import ticker");
-    await self.pyodide.runPython("import numpy as np");
+   
+    // import base64, io and named tupples
     await self.pyodide.runPython("import io, base64");
     await self.pyodide.runPython("from collections import namedtuple");
-    document.getElementById("run").classList.remove("hidden");
+ 
+    // load matplotlib if we are not on safari  
+    if (browser != 'safari') {
+        await self.pyodide.loadPackage('matplotlib');
+        await self.pyodide.runPython("import matplotlib.pyplot as plt")
+        await self.pyodide.runPython("from matplotlib import ticker");
+    }  
+   
+    // for safari, load numpy
+    // numpy is indirectly loaded with matplotlib on other browsers
+    if (browser == 'safari' ) {
+        await self.pyodide.loadPackage('numpy');   
+        // hide any graph elements on safari
+        hideGraphElements();
+    } 
+
+    await self.pyodide.runPython("import numpy as np");
+   
     let code = await readCode("./python/adTax.py");
     let html_table = await readCode("./python/htmlTable.py")
+   
     pyodide.runPython(html_table);
     pyodide.runPython(code);
+
+    if (browser != 'safari') {
+        let graphs = await readCode("./python/graphs.py");
+        pyodide.runPython(graphs);
+    }
+
+    document.getElementById("run").classList.remove("hidden");
+
 }
 
 loadPyodideAndPackages()
@@ -33,14 +62,18 @@ function executeCode() {
         pyodide.runPython("table_total = table_revenue_tax(b, r)");
         document.getElementById("rev-tax-table-calculated").innerHTML=pyodide.globals.get("table_total");
         
-        pyodide.runPython("calculated_graph = us_fig(b,r)");
-        document.getElementById("avg-tax-rate-calculated").src=pyodide.globals.get("calculated_graph");
+        if (browser != 'safari') {
+            pyodide.runPython("calculated_graph = us_fig(b,r)");
+            document.getElementById("avg-tax-rate-calculated").src=pyodide.globals.get("calculated_graph");
+        } 
+
         pyodide.runPython("split_1 = split(1,b,r)");
         pyodide.runPython("split_2 = split(2,b,r)");
         pyodide.runPython("split_4 = split(4,b,r)");
         document.getElementById("split-1").innerHTML=pyodide.globals.get("split_1");
         document.getElementById("split-2").innerHTML=pyodide.globals.get("split_2");
         document.getElementById("split-4").innerHTML=pyodide.globals.get("split_4");
+
     } catch(err) {
         message = '<div class="mb-2">Oops! There seems to be a problem with the code you submitted. Please take a look to see if there is a line number in the error message below. If so, try editing the line and click the run button again. For example, be sure that you have not added a space before either of the variables, b and r.</div>'
         showPythonError(err, message, "us_estimates_error");
@@ -56,3 +89,45 @@ function showPythonError(err, message, div_id){
 document.getElementById('run').addEventListener('click', function () {
     executeCode();
 });
+
+function hideGraphElements() {
+    document.getElementById('avg-tax-rate-calculated').remove();
+    document.getElementById('avg-tax-rate-calculated-container').innerHTML = '<div class="error">To see an additional graph created by matplotlib, please use Chrome, Firefox or Edge on a laptop.</div>'
+}
+
+// detect mobile
+// https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+function detectmob() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+       return true;
+     }
+    else {
+       return false;
+     }
+}
+// get browser
+// https://stackoverflow.com/questions/5916900/how-can-you-detect-the-version-of-a-browser
+function get_browser() {
+    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+        return {name:'IE',version:(tem[1]||'')};
+        }   
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR|Edge\/(\d+)/)
+        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+        }   
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+    return {
+      name: M[0],
+      version: M[1]
+    };
+ }
