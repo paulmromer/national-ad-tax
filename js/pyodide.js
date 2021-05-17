008a6ca022088cbf2057f21e967c5d661ad29965
Paul Romer <paul@paulromer.net>
2021-05-17 10:58:08 -0400

    add error handling for user submitted python

--- js/pyodide.js
+++ js/pyodide.js
@@ -21,20 +21,33 @@ async function readCode(file) {
 
 function executeCode() {
     input = editor.getDoc().getValue();
-    pyodide.runPython(input);
-    pyodide.runPython("table_marg = table_marg_rates(b, r)");
-    pyodide.runPython("table_rev_tax = table_revenue_tax(b, r)");
-    document.getElementById("results").classList.remove("hidden")
-    document.getElementById("marginal-rate-table-calculated").innerHTML=pyodide.globals.get("table_marg");
-    document.getElementById("rev-tax-table-calculated").innerHTML=pyodide.globals.get("table_rev_tax");
-    pyodide.runPython("calculated_graph = us_fig(b,r)");
-    document.getElementById("avg-tax-rate-calculated").src=pyodide.globals.get("calculated_graph");
-    pyodide.runPython("split_1 = split(1,b,r)");
-    pyodide.runPython("split_2 = split(2,b,r)");
-    pyodide.runPython("split_4 = split(4,b,r)");
-    document.getElementById("split-1").innerHTML=pyodide.globals.get("split_1");
-    document.getElementById("split-2").innerHTML=pyodide.globals.get("split_2");
-    document.getElementById("split-4").innerHTML=pyodide.globals.get("split_4");
+    try {
+        // clear any error messages from previous runs (if any)
+        document.getElementById('us_estimates_error').classList.add('hidden');
+        // run python code
+        pyodide.runPython(input);
+        pyodide.runPython("table_marg = table_marg_rates(b, r)");
+        pyodide.runPython("table_total = table_revenue_tax(b, r)");
+        document.getElementById("results").classList.remove("hidden")
+        document.getElementById("marginal-rate-table-calculated").innerHTML=pyodide.globals.get("table_marg");
+        document.getElementById("rev-table-calculated").innerHTML=pyodide.globals.get("table_total");
+        pyodide.runPython("calculated_graph = us_fig(b,r)");
+        document.getElementById("avg-tax-rate-calculated").src=pyodide.globals.get("calculated_graph");
+        pyodide.runPython("split_1 = split(1,b,r)");
+        pyodide.runPython("split_2 = split(2,b,r)");
+        pyodide.runPython("split_4 = split(4,b,r)");
+        document.getElementById("split-1").innerHTML=pyodide.globals.get("split_1");
+        document.getElementById("split-2").innerHTML=pyodide.globals.get("split_2");
+        document.getElementById("split-4").innerHTML=pyodide.globals.get("split_4");
+    } catch(err) {
+        showPythonError(err, "us_estimates_error");
+    }
+}
+
+function showPythonError(err , div_id){
+    el =  document.getElementById(div_id);
+    el.classList.remove('hidden');
+    el.innerHTML = err.message;
 }
 
 document.getElementById('run').addEventListener('click', function () {
