f9be29ebb452ffb738a5629b934505e02686d5f0
Paul Romer <paul@paulromer.net>
2021-05-26 17:56:53 -0400

    remove logging

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 
-
 if (detectSafariOrMobile()) {
-  console.log("yes safari");
-  // document.getElementById("avg-tax-rate-calculated").src=wm.data[4]
   document.getElementById("results").classList.remove("hidden");
   document.getElementById("placeholder").classList.add("hidden");
-  document.getElementById("split-1").innerHTML="$22.5";
-  document.getElementById("split-2").innerHTML="$15.0";
-  document.getElementById("split-diff").innerHTML="$7.5";
+  document.getElementById("split-1").innerHTML = "$22.5";
+  document.getElementById("split-2").innerHTML = "$15.0";
+  document.getElementById("split-diff").innerHTML = "$7.5";
 
 }
 else {
@@ -23,8 +20,8 @@ else {
   document.getElementById("editor-div").classList.remove("hidden")
 
   editor.setSize("100%", "350px");
-    
-    let initial_editor_text = `
+
+  let initial_editor_text = `
 # These are the brackets and marginal rates used above.
 #
 #b = [0,    5,    10,   15,    20,   25,    30,   35,    40,   50,    60]
@@ -44,88 +41,74 @@ r = [0, 0.10, 0.20, 0.30, 0.40, 0.50]
 #r = [0, 0.05, 0.125, 0.20, 0.275, 0.35, 0.425, 0.50, 0.575, 0.65, 0.725]
 #r = [ min(rate * f, 1.0) for rate in r]
 
-# Or type in any other system of brackets and rates that you want to explore.` 
+# Or type in any other system of brackets and rates that you want to explore.`
 
-  editor.getDoc().setValue(initial_editor_text); 
-  console.log("initial_editor_text")
-  myWorker.onmessage = function(wm) {
-  if (wm.data[0] == "worker is ready") {
-    console.log("worker is ready msg received by main");
-    document.getElementById("run").classList.remove("hidden");
-    document.getElementById("placeholder").classList.add("hidden");
-    document.getElementById('run').addEventListener('click', function () {
-      let current_editor_text = editor.getDoc().getValue();
-      myWorker.postMessage(current_editor_text);
-      console.log("run clicked first time");
-    });
-    
-  }
-  else{
-    try {
-      // clear placeholder and any error messages from previous runs 
-      document.getElementById('us_estimates_error').classList.add('hidden');
-      document.getElementById('placeholder').classList.add('hidden');
-      document.getElementById('editor-instructions').classList.remove('hidden');
-      document.getElementById("you-clicked").classList.remove('hidden');
-
-      document.getElementById("marginal-rate-table-calculated").innerHTML= wm.data[1];
-      
-      document.getElementById("rev-table-calculated").innerHTML=wm.data[2];
-      document.getElementById("tax-table-calculated").innerHTML=wm.data[3];
-      document.getElementById("avg-tax-rate-calculated").src=wm.data[4];
-      
-      // if (detectSafariOrMobile() == false ) {
-      //     document.getElementById("avg-tax-rate-calculated").src=wm.data[4];
-      // } 
-
-      document.getElementById("split-1").innerHTML=wm.data[5];
-      document.getElementById("split-2").innerHTML=wm.data[6];
-      document.getElementById("split-diff").innerHTML=wm.data[7];
+  editor.getDoc().setValue(initial_editor_text);
+  myWorker.onmessage = function (wm) {
+    if (wm.data[0] == "worker is ready") {
       document.getElementById("run").classList.remove("hidden");
-      document.getElementById("results").classList.remove("hidden");
-
+      document.getElementById("placeholder").classList.add("hidden");
       document.getElementById('run').addEventListener('click', function () {
-        let current_editor_text = editor.getDoc().getValue()
+        let current_editor_text = editor.getDoc().getValue();
         myWorker.postMessage(current_editor_text);
-        console.log("run clicked subsequent times");
       });
-    }
-    catch(err) {
-      message = '<div class="mb-2">Oops! There seems to be a problem with the code you submitted. Please take a look to see if there is a line number in the error message below. If so, try editing the line and click the run button again. For example, be sure that you have not added a space before either of the variables, b and r.</div>'
-      showPythonError(err, message, "us_estimates_error");
-    }
 
     }
-  } 
+    else {
+      try {
+        // clear placeholder and any error messages from previous runs 
+        document.getElementById('us_estimates_error').classList.add('hidden');
+        document.getElementById('placeholder').classList.add('hidden');
+        document.getElementById('editor-instructions').classList.remove('hidden');
+        document.getElementById("you-clicked").classList.remove('hidden');
 
-  myWorker.onmessageerror = function(error) {
+        document.getElementById("marginal-rate-table-calculated").innerHTML = wm.data[1];
+
+        document.getElementById("rev-table-calculated").innerHTML = wm.data[2];
+        document.getElementById("tax-table-calculated").innerHTML = wm.data[3];
+        document.getElementById("avg-tax-rate-calculated").src = wm.data[4];
+        document.getElementById("split-1").innerHTML = wm.data[5];
+        document.getElementById("split-2").innerHTML = wm.data[6];
+        document.getElementById("split-diff").innerHTML = wm.data[7];
+        document.getElementById("run").classList.remove("hidden");
+        document.getElementById("results").classList.remove("hidden");
+
+        document.getElementById('run').addEventListener('click', function () {
+          let current_editor_text = editor.getDoc().getValue()
+          myWorker.postMessage(current_editor_text);
+          console.log("run clicked subsequent times");
+        });
+      }
+      catch (err) {
+        message = '<div class="mb-2">Oops! There seems to be a problem with the code you submitted. Please take a look to see if there is a line number in the error message below. If so, try editing the line and click the run button again. For example, be sure that you have not added a space before either of the variables, b and r.</div>'
+        showPythonError(err, message, "us_estimates_error");
+      }
+
+    }
+  }
+
+  myWorker.onmessageerror = function (error) {
     console.log(error);
   };
 }
 
-
-// function hideGraphElements() {
-//   document.getElementById('avg-tax-rate-calculated').remove();
-//   document.getElementById('avg-tax-rate-calculated-container').innerHTML = '<div class="error">To see a new graph based on your changes to the tax brackets and rates, please come back to this page from a desktop machine using Chrome, Firefox or Edge.</div>'
-// }
-
-function showPythonError(err, message, div_id){
-  el =  document.getElementById(div_id);
+function showPythonError(err, message, div_id) {
+  el = document.getElementById(div_id);
   el.classList.remove('hidden');
   el.innerHTML = message + err.message;
 }
 
 
-function detectSafariOrMobile(){
+function detectSafariOrMobile() {
   // get the browser
-  const browser = get_browser().name.toLowerCase();  
+  const browser = get_browser().name.toLowerCase();
 
   if (browser == 'safari') {
-      return true;
+    return true;
   }
-  
+
   if (detectmob()) {
-      return true;
+    return true;
   }
 
   return false;
@@ -133,39 +116,37 @@ function detectSafariOrMobile(){
 
 // detect mobile
 // https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
-function detectmob() { 
-  if( navigator.userAgent.match(/Android/i)
-  || navigator.userAgent.match(/webOS/i)
-  || navigator.userAgent.match(/iPhone/i)
-  || navigator.userAgent.match(/iPad/i)
-  || navigator.userAgent.match(/iPod/i)
-  || navigator.userAgent.match(/BlackBerry/i)
-  || navigator.userAgent.match(/Windows Phone/i)
-  ){
-     return true;
-   }
+function detectmob() {
+  if (navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    return true;
+  }
   else {
-     return false;
-   }
+    return false;
+  }
 }
 // get browser
 // https://stackoverflow.com/questions/5916900/how-can-you-detect-the-version-of-a-browser
 function get_browser() {
-  var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
-  if(/trident/i.test(M[1])){
-      tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
-      return {name:'IE',version:(tem[1]||'')};
-      }   
-  if(M[1]==='Chrome'){
-      tem=ua.match(/\bOPR|Edge\/(\d+)/)
-      if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-      }   
-  M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-  if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+  var ua = navigator.userAgent, tem, M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if (/trident/i.test(M[1])) {
+    tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return { name: 'IE', version: (tem[1] || '') };
+  }
+  if (M[1] === 'Chrome') {
+    tem = ua.match(/\bOPR|Edge\/(\d+)/)
+    if (tem != null) { return { name: 'Opera', version: tem[1] }; }
+  }
+  M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+  if ((tem = ua.match(/version\/(\d+)/i)) != null) { M.splice(1, 1, tem[1]); }
   return {
     name: M[0],
     version: M[1]
   };
 }
-
-console.log("after defn of dectedSafariOrMobile");
--- js/worker.js
+++ js/worker.js
@@ -6,33 +6,21 @@ async function loadImportRun() {
     await runPyodideScript();
     await loadPyodideAsync();
     await pyodide.loadPackage('matplotlib');
-    console.log("matplotlib fully loaded");
     await pyodide.runPython("import io, base64, os");
-    console.log("io, base64, os imported");
     await pyodide.runPython("os.environ['MPLBACKEND'] = 'AGG'");
-    // await pyodide.runPython("sys.setrecursionlimit(10**6)");
-    console.log("backend changed to AGG");
     await pyodide.runPython("import matplotlib");
-    console.log("matplotlib imported");
     await pyodide.runPython("import matplotlib.pyplot as plt");
-    console.log("pyplot imported");
     await pyodide.runPython("import numpy as np");
     await self.pyodide.runPython("from collections import namedtuple");
     await self.pyodide.runPython("from matplotlib import ticker");
-    console.log("numpy, namedtuple, ticker imported")
     postMessage(
         ["worker is ready", "", "", "", "","",""]
     )
     onmessage = async function(mm) {
         pyodide.runPython(mm.data); 
-        console.log("Message received from main.js");
-        pyodide.runPython(await (await fetch('https://adtax.paulromer.net/python/tablesGraphsSplit.py')).text());
-        console.log("tablesGraphsSplit.py fetched and executed")
-        // pyodide.runPython(await (await fetch('http://127.0.0.1:8000/python/htmlTable.py')).text());
-        // pyodide.runPython(await (await fetch('http://127.0.0.1:8000/python/graphs.py')).text());
+        pyodide.runPython(await (await fetch('/python/tablesGraphsSplit.py')).text());
         pyodide.runPython("check_msg = check_tax_schedule(b,r)"); 
         check_msg = pyodide.globals.get("check_msg");
-        // let check_msg = ""
         if (check_msg.length > 0) {
             postMessage(
                 [check_msg, "", "", "", "", "", "", ""]
@@ -41,13 +29,9 @@ async function loadImportRun() {
         else {
             
             pyodide.runPython("table_marg_rates_result = table_marg_rates(b, r)");
-            console.log("marg_rates")
             pyodide.runPython("table_revenue_result = table_revenue(b, r)");
-            console.log("revenue")
             pyodide.runPython("table_tax_result = table_tax(b, r)"); 
-            console.log("tax")   
             pyodide.runPython("us_fig_result = us_fig(b, r)");
-            console.log("us_fig")
             pyodide.runPython(
                 "split_1_result = '$'+format(split(50, 1, b, r), '3.1f')"
                 )
@@ -57,10 +41,6 @@ async function loadImportRun() {
             pyodide.runPython(
                 "split_diff_result = '$'+format(split(50, 1, b, r) - split(50, 2, b, r), '3.1f')"
                 )
-            // pyodide.globals.get("table_marg_rates_result"),
-            // pyodide.globals.get("table_revenue_result"),
-            // pyodide.globals.get("table_tax_result"),
-            // pyodide.globals.get("us_fig_result"),
             split_1_result = pyodide.globals.get("split_1_result"),
             split_2_result = pyodide.globals.get("split_2_result"),
             split_diff_result = pyodide.globals.get("split_diff_result")
@@ -87,37 +67,4 @@ async function runPyodideScript() {
 async function loadPyodideAsync() {
     await loadPyodide({ indexURL : 'https://cdn.jsdelivr.net/pyodide/v0.17.0/full/' });
 }
-
-// async function loadCode(file) {
-//     c = fetch(file).text();
-//     return c
-// }
-
-// async function loadCode(file) {
-//     return  (await (await fetch(file)).text());
-// }
-
-
-// function loadcode(){
-//     snippet = `def valid_tax_schedule(b,r):
-//     invalid = """The tax schedule you submitted fails to meet one of these requirements: \n
-//     1. b[0] must equal 0
-//     2. b and r must have the same length 
-//     3. b must be monotonically increasing"""
-//     if not len(b) == len(r):
-//         return invalid
-
-//     if not b[0] == 0:
-//         return invalid
-
-//     b_diff = [b[j+1] - b[j] for j in range(len(b)-2)]
-//     for diff in b_diff:
-//         if diff <= 0:
-//             return invalid
-//     return ""`
-//     return snippet
-// }
-
-
-
 
